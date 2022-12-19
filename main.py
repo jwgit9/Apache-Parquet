f963ff5691bc2ee421bc5b87547301e29b89798c
jwgit9 <39379864+jwgit9@users.noreply.github.com>
2022-12-19 15:31:54 -0800

    parquet file to csv bug fixes and code refraction
    - fixed bug with conversion of parquet to csv files
    - refactored code to be more organized

--- main.py
+++ main.py
@@ -1,72 +1,76 @@
 import pandas as pd
 import os
+import pathlib  # to find file extension
+import shutil  # to copy files
+# tests were done with pyarrow as the parquet engine, <pip install pyarrow>
 
-# tests were done with pyarrow as the parquet engine
-# pip install pyarrow
 
+def csv_to_parquet(file_name):
+    print(f"\nProcessing: {file_name}")
 
-def csv_to_parquet(file_name, input_path, output_path):
-    print(f"\nReading in: {file_name}")
+    input_path = f"{INPUT_CSV_LOCATION}{file}"
+    if pathlib.Path(file).suffix.lower() == '.csv':
+        output_path = f"{OUTPUT_PARQUET_LOCATION}{file.replace('.csv','').replace('.CSV', '')}.parquet"
 
-    try:
-        df = pd.read_csv(input_path)
         try:
-            df.to_parquet(output_path, engine="pyarrow")
-            print(f"----- Converted to parquet")
+            df = pd.read_csv(input_path)
+            try:
+                df.to_parquet(output_path, engine="pyarrow", index=False)
+                print(f"----- Converted to parquet")
+            except Exception as e:
+                print(f"----------------Error converting to parquet | {e}")
         except Exception as e:
-            print(f"----------------Error converting to parquet | {e}")
-    except Exception as e:
-        print(f"-----------------Error reading csv | {e}")
+            print(f"-----------------Error reading csv | {e}")
+    else:
+        print(f"----- Not a csv file, copying file directly")
+        shutil.copy(input_path, OUTPUT_PARQUET_LOCATION)
 
 
-def parquet_to_csv(file_name, input_path, output_path):
-    print(f"\nReading in: {file_name}")
-    try:
-        df = pd.read_parquet(input_path)
+def parquet_to_csv(file_name):
+    print(f"\nProcessing: {file_name}")
+
+    input_path = f"{INPUT_PARQUET_LOCATION}{file}"
+    if pathlib.Path(file).suffix.lower() == '.parquet':
+        output_path = f"{OUTPUT_CSV_LOCATION}{file.replace('.parquet', '.csv')}"
+
         try:
-            df.to_parquet(output_path)
-            print(f"----- Converted to csv")
+            df = pd.read_parquet(input_path)
+            try:
+                df.to_csv(output_path, index=False)
+                print(f"----- Converted to csv")
+            except Exception as e:
+                print(f"----------------Error converting to csv | {e}")
         except Exception as e:
-            print(f"----------------Error converting to csv | {e}")
-    except Exception as e:
-        print(f"-----------------Error reading parquet | {e}")
+            print(f"-----------------Error reading parquet | {e}")
+    else:
+        print(f"----- Not a parquet file, copying file directly")
+        shutil.copy(input_path, OUTPUT_CSV_LOCATION)
 
 
 if __name__ == "__main__":
-
     # used absolute paths here since we have different operating systems
-    input_csv = "C:/Apache-Parquet/input-data/"
-    parquet_files = "C:/Apache-Parquet/output-parquet/"  # Parquet files converted from csv
-    csv_files = "C:/Apache-Parquet/output-csv/"  # CSV files converted back from csv
+    # don't forget the '/' at the end of the file path
 
-    if not os.path.exists(input_csv):
-        print("Error! No Input data!")
+    INPUT_CSV_LOCATION = "C:/Apache-Parquet/input-data/"  # csv files to convert to parquet
+    OUTPUT_PARQUET_LOCATION = "C:/Apache-Parquet/output-parquet/"  # parquet files converted from csv
+
+    INPUT_PARQUET_LOCATION = "C:/Apache-Parquet/output-parquet/"  # parquet files to convert back to csv
+    OUTPUT_CSV_LOCATION = "C:/Apache-Parquet/output-csv/"  # csv files converted from parquet
+
+    # sanity checks
+    if not os.path.exists(INPUT_CSV_LOCATION) and not os.path.exists(INPUT_PARQUET_LOCATION):
+        print("Error! No input data of any kind!")
         exit()
-    if not os.path.exists(parquet_files):
-        os.makedirs(parquet_files)
-    if not os.path.exists(csv_files):
-        os.makedirs(csv_files)
+    if OUTPUT_PARQUET_LOCATION and not os.path.exists(OUTPUT_PARQUET_LOCATION):
+        os.makedirs(OUTPUT_PARQUET_LOCATION)
+    if OUTPUT_CSV_LOCATION and not os.path.exists(OUTPUT_CSV_LOCATION):
+        os.makedirs(OUTPUT_CSV_LOCATION)
 
     # Converts csv data to parquet files
-    for file in os.listdir(input_csv):
-        input_file_path = f"{input_csv}{file}"
-        output_file_path = f"{parquet_files}{file.replace('.csv','').replace('.CSV', '')}.parquet"
-
-        csv_to_parquet(file, input_file_path, output_file_path)
+    for file in os.listdir(INPUT_CSV_LOCATION):
+        csv_to_parquet(file)
 
     # Reads the parquet files and converts back to csv files
-    for file in os.listdir(parquet_files):
-        input_file_path = f"{parquet_files}{file}"
-        output_file_path = f"{csv_files}{file.replace('.parquet', '')}.csv"
+    for file in os.listdir(INPUT_PARQUET_LOCATION):
+        parquet_to_csv(file)
 
-        parquet_to_csv(file, input_file_path, output_file_path)
-
-    # -------------- Code below this line works, code above this line causes faulty csv files for some reason -------------------
-
-    data = [['apple', 10], ['orange', 15], ['peach', 14]]
-
-    df_test = pd.DataFrame(data, columns=['Fruit', 'Amount'])
-    df_test.to_parquet("dataframe.parquet", engine="pyarrow")  # pip install pyarrow
-
-    df_test_2 = pd.read_parquet("dataframe.parquet")
-    df_test_2.to_csv("filename.csv")
